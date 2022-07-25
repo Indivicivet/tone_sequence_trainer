80ac383587dc726ce0180df4a63975a125f9f94d
Indivicivet <indivicivet@gmail.com>
2022-07-25 19:34:53 +0100

    implement playing a sequence of tones

--- tone_sequence_trainer.js
+++ tone_sequence_trainer.js
@@ -3,37 +3,60 @@
 // todo :: less bad
 var tones = {
 	1: [[0, 1]],  // high
-	2: [[0, 0], [1, 1]],  // up
+	2: [[0.1, 0], [0.9, 1]],  // up
 	3: [[0, 0.3], [0.5, 0], [1, 0.5]],  // low and wobbly
-	4: [[0, 1], [1, 0]],  // down
+	4: [[0.1, 1], [0.9, 0]],  // down
 }
 // todo :: tones for specific pairs
 // todo :: no tone
 
 
-function play_tone(pitches) {
+function play_tone_seq(...tones) {
 	var context = new AudioContext()
 	var o = context.createOscillator()
-	var length = 0.5
+	var gain = context.createGain()
+	
+	var vocalize_duration = 0.3
+	var pause_duration = 0.1
+	
 	var min_pitch = 220
 	var max_pitch = 440
-	var pitch = o.frequency
-	for (i = 0; i < pitches.length; i++) {
-		pitch.linearRampToValueAtTime(
-			pitches[i][0] * (max_pitch - min_pitch) + min_pitch,
-			pitches[i][1] * length
-		)
+	
+	function rel_pitch_to_hz(rel) {
+		return rel * (max_pitch - min_pitch ) + min_pitch
 	}
-	o.connect(context.destination)
+	
+	var t = 0
+	for (j = 0; j < tones.length; j++){
+		pitches = tones[j]
+		gain.gain.setValueAtTime(1, t)
+		o.frequency.setValueAtTime(rel_pitch_to_hz(pitches[0][1]), t)
+		for (i = 0; i < pitches.length; i++) {
+			o.frequency.linearRampToValueAtTime(
+				rel_pitch_to_hz(pitches[i][1]),
+				t + pitches[i][0] * vocalize_duration
+			)
+		}
+		t += vocalize_duration
+		gain.gain.setValueAtTime(0, t)
+		if (j < tones.length - 1) {
+			t += pause_duration
+		}
+	}
+	o.connect(gain).connect(context.destination)
 	o.start(0)
-	o.stop(length)
+	o.stop(t)
 }
 
 
-function choose_and_play_tone() {
-	var tone_name = 3  // todo
-	play_tone(tones[tone_name])
+function choose_and_play_seq() {
+	play_tone_seq(
+		tones[1],
+		tones[2],
+		tones[3],
+		tones[4],
+	)
 }
 
 
-document.getElementById("begin").onclick = choose_and_play_tone
+document.getElementById("begin").onclick = choose_and_play_seq
