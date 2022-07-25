e0ec465229e22773aabe64ec851af6ca78cb9297
Indivicivet <indivicivet@gmail.com>
2022-07-25 20:02:47 +0100

    highlight playing marker (in hacky way)

--- tone_sequence_trainer.js
+++ tone_sequence_trainer.js
@@ -11,13 +11,16 @@ var tones = {
 // todo :: no tone
 
 
+var markers = document.getElementsByClassName("marker")
+
+var vocalize_duration = 0.3
+var pause_duration = 0.1
+
+
 function play_tone_seq(...tones) {
 	var context = new AudioContext()
 	var o = context.createOscillator()
 	var gain = context.createGain()
-	
-	var vocalize_duration = 0.3
-	var pause_duration = 0.1
 	
 	var min_pitch = 240
 	var max_pitch = 300
@@ -49,7 +52,21 @@ function play_tone_seq(...tones) {
 }
 
 
+function unplayingAllMarkers() {
+	for (i = 0; i < markers.length; i++) {
+		markers[i].classList.remove("playing")
+	}
+}
+
+
+function playingMarker(i) {
+	console.log(i)
+	markers[i].classList.add("playing")
+}
+
+
 function choose_and_play_seq() {
+	// todo :: tone seq based on # of markers
 	var tone_seq = [1, 2, 3, 4, 1]
 	play_tone_seq(
 		...tone_seq.map(t => tones[t])
@@ -58,6 +75,22 @@ function choose_and_play_seq() {
 	// in sync with the tone playing, isn't it?
 	// consider refactor. (but I want something playable first)
 	
+	// duplicated time tracking code :(
+	t = 0
+	for (i = 0; i < markers.length; i++) {
+		// todo :: do I actually have to do this...?
+		var f = [
+			function(){ playingMarker(0) },
+			function(){ playingMarker(1) },
+			function(){ playingMarker(2) },
+			function(){ playingMarker(3) },
+			function(){ playingMarker(4) },
+		]
+		setTimeout(f[i], t * 1000)
+		t += vocalize_duration
+		setTimeout(unplayingAllMarkers, t * 1000)
+		t += pause_duration
+	}
 }
 
 
