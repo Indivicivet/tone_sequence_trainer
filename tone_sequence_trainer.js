61735a7341eef47718669ac9693d7b7466b680fd
Indivicivet <indivicivet@gmail.com>
2022-07-25 19:15:49 +0100

    wobble the pitch :)

--- tone_sequence_trainer.js
+++ tone_sequence_trainer.js
@@ -2,6 +2,9 @@
 function play_seq() {
 	var context = new AudioContext()
 	var o = context.createOscillator()
+	var pitch = o.frequency
+	pitch.linearRampToValueAtTime(220, 0.2)
+	pitch.linearRampToValueAtTime(440, 0.5)
 	o.connect(context.destination)
 	o.start(0)
 	o.stop(0.5)
